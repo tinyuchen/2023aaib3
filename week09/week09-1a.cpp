cf3d0863ddd21940cea685566d0ff1bea9f9f37c
tinyuchen <145088468+tinyuchen@users.noreply.github.com>
2024-11-06 12:03:22 +0800

    week09

---
+++ week09/week09-1a.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int M=matrix.size();
+        int N=matrix.size();
+        int i=0,j=0,dir=0;  //0 right, 1 down, 2 left, 3 up
+        int dI[4]={0,1,0,-1};   //value of move
+        int dJ[4]={1,0,-1,0};   //value of move
+        
+        vector<int> ans;
+        ans.push_back(matrix[i][j]);    //把答案送進去ans
+        i+=dI[dir];
+        j+=dJ[dir];
+        ans.push_back(matrix[i][j]);
+        i+=dI[dir];
+        j+=dJ[dir];
+        ans.push_back(matrix[i][j]);
+        return ans;
+    }
+};
